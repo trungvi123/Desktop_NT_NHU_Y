551135e78942f98023eac8fb93e6bc59a487280d
trungvi123 <trungvi123@gmail.com>
2023-05-11 08:16:27 +0700

    update animate and tab select

--- public/desktop.js
+++ public/desktop.js
@@ -1,3 +1,13 @@
+
+$(document).ready(function(){
+    $(".service-menu-list a").click(function(){
+      $(this).tab('show');
+    });
+    $(".product-menu-list a").click(function(){
+        $(this).tab('show');
+      });
+  });
+
 var nav_responsive = document.querySelector('.nav-responsive')
         var content_nav = document.querySelector('.content-nav')
         var menuChildProductBtn = document.querySelector('.menuChildProductBtn')
@@ -49,42 +59,72 @@ var nav_responsive = document.querySelector('.nav-responsive')
 
 // animation
 
-var productCard = document.querySelectorAll('.product-card')
-var serviceCard = document.querySelectorAll('.service-card')
+// var productCard = document.querySelectorAll('.product-card')
+// var serviceCard = document.querySelectorAll('.service-card')
 var feedbackList = document.querySelector('.feedback-list')
 
 
+
+var circle = document.querySelector('.circle-up');
+
+
 window.onscroll = function (){
-    // tùy tnhf duyệt mà sài body hay element
-    console.log( document.documentElement.scrollWidth,);
+//     // tùy tnhf duyệt mà sài body hay element
+//     console.log( document.documentElement.scrollWidth,);
+
+    if(document.body.scrollTop > 621 || document.documentElement.scrollTop > 621){
+        circle.style.opacity = "0.3";
+        circle.style.display =  "block";
+    }else {
+        circle.style.opacity = "0";
+        circle.style.display =  "none";
+    }
+
     if(document.documentElement.scrollTop > 680 && document.documentElement.scrollTop < 1450 ){
-        productCard.forEach((item)=>{
-            item.classList.remove('transformLR')
-        })
-        serviceCard.forEach((item)=>{
-            item.classList.remove('transformRL')
-        })
+        $('.product-card').removeClass('magictime swap')
+        $('.service-card').removeClass('magictime twisterInDown')
+       
         feedbackList.classList.remove('transformLRFull')
     }
 
     if(document.body.scrollTop > 1450 || document.documentElement.scrollTop > 1450 && document.documentElement.scrollTop < 2600){
-       
-        productCard.forEach((item)=>{
-            item.classList.add('transformLR')
-        })
-
+        $('.product-card').addClass('magictime swap')
     }
     
     if(document.body.scrollTop > 2600 || document.documentElement.scrollTop > 2600 && document.documentElement.scrollTop < 3500){
-        serviceCard.forEach((item)=>{
-            item.classList.add('transformRL')
-        })
-   
+        $('.service-card').addClass('magictime twisterInDown')
     }
 
-    if(document.body.scrollTop > 2600 || document.documentElement.scrollTop > 3700){
-            feedbackList.classList.add('transformLRFull')
-    }
+//     if(document.body.scrollTop > 2600 || document.documentElement.scrollTop > 3700){
+//             feedbackList.classList.add('transformLRFull')
+//     }
 }
+
+    if(circle){
+        circle.onclick= function topFunction () {
+            document.body.scrollTop =0;
+            document.documentElement.scrollTop =0;
+        }
+    }
+
+
+// animate text
+var options = {
+    duration: 3000,
+    iterations: Infinity,
+    id: 'myAnimation'
+  };
+
+window.animatelo.rubberBand('.rubberBand',options);
+
+// ANIMATE VIP
+
+setInterval(()=>{
+    $('.criteria__circle').addClass('vanishIn magictime');
+},4000)
+
+setInterval(()=>{
+    $('.criteria__circle').removeClass('vanishIn magictime');
+},6000)
 
 
